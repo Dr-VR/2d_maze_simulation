06971619d87dcda2599ad3a0a309d97eada22d43
sfrutos <44537981+sfrutos@users.noreply.github.com>
2020-04-06 16:36:35 -0700

    First commit
    Added 2D maze environment and follow mouse script

---
+++ maze_test/Assets/Scripts/followMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class followMouse : MonoBehaviour
+{
+    public float distance = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 new mousePosition = Input.mousePosition;
+        Vector3 mousePosition = new Vector3();
+        mousePosition = Input.mousePosition;
+        mousePosition.z = distance;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Debug.Log("Coordinates : " + mousePosition);
+        Debug.Log("Coordinates : " + transform.position);
+
+    }
+}
