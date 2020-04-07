a7da6e24e4e18be8d085a49a5a2afee104f3cb4e
sfrutos <44537981+sfrutos@users.noreply.github.com>
2020-04-06 19:31:23 -0700

    Update to print coordinates and add difference
    Updated print to log syntax and added difference of values as test for when adding next gameObject

--- maze_test/Assets/Scripts/followMouse.cs
+++ maze_test/Assets/Scripts/followMouse.cs
@@ -20,18 +20,20 @@ public class followMouse : MonoBehaviour
         mousePosition.z = distance;
         transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
 
-   
         //Testing different ways to print as it was not printing originally 
         //Debug.Log("TEST PRINT");
         //print("TEST PRINT");
         //Debug.LogWarning("TEST PRINT");
 
         //Prints out x, y, and z mouse coordinates to Unity console
-        Debug.LogError("values: " + transform.position);
-        //Prints out x and y coordinates to unity console
-        Debug.LogError("x val: " + transform.position.x + "y val: " + transform.position.y);
+        //Debug.LogWarning("values: " + transform.position);
+        //Prints out x and y coordinates to unity console -- Error shouldnt print but my system was not working with Debug.Log
+        Debug.LogWarning("x val: " + transform.position.x + "y val: " + transform.position.y + "diff val: " + (transform.position.x - transform.position.y));
 
-
-
+        // Correct syntax, but my system was not printing out correctly
+        //Debug.Log("X Value: ");
+        //Debug.Log(transform.position.x);
+        //Debug.Log(" Y Value: ");
+        //Debug.Log(transform.position.y);
     }
 }
